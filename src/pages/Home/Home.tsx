ee0961d8c4501cfdae97f4ce5275cd75cc58cd4b
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-22 19:11:56 -0300

    ✨ feat: agora é possivel filtrar os posts por titulo

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,11 +9,19 @@ import http from '../../service/api'
 const Home = () => {
 
   const [posts, setPosts] = useState<IPost[]>([])
+  const [searchText, setSearchText] = useState("")
 
   useEffect(() => {
     http.get<IPost[]>('posts')
       .then(resposta => setPosts(resposta.data.slice(0, 2)))
   }, [])
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  const filteredPosts = posts.filter(post => post.title.includes(searchText));
+
 
   return (
     <div className="container-sm">
@@ -23,11 +31,11 @@ const Home = () => {
         <p className="roboto">A blog about food, experiences, and recipes.</p>
       </div>
       <div className="container-sm d-flex justify-content-center">
-        <Input />
+        <Input value={searchText} onChange={handleSearchChange} />
       </div>
       <section className='container-sm pt-5 pb-5'>
         <div className="row row-cols-1 row-cols-md-2 g-5 justify-content-around">
-          {posts.map(post => {
+          {filteredPosts.map(post => {
             return (
               <Link to={`/post/${post.id}`} className="react-link"><Card key={post.id} title={post.title} body={post.body.slice(0, 100)} /></Link>
             )
