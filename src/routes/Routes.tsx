d698139521fbe6ce3ed5984c73e5a36943cdf3d8
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-22 15:56:53 -0300

    ✨ feat: add nova rota page post

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route } from 'react-router'
 import { BrowserRouter, Routes as Switch} from 'react-router-dom'
 import Home from '../pages/Home/Home'
+import PagePost from '../pages/PagePost/PagePost'
 
 const Routes = () => {
   return (
     <BrowserRouter>
     <Switch>
         <Route path='/' element={<Home/>}/>
+        <Route path='post/:id' element={<PagePost/>}/>
     </Switch>
     </BrowserRouter>
   )
