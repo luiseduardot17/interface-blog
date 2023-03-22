bfcb75cdd97aecd00fa8a957b358a9db692fb104
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-22 03:52:12 -0300

    ✨ feat: import navbar e input.  req get dos posts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Input from '../../components/Input/Input'
+import Navbar from '../../components/Navbar/Navbar'
+import IPost from '../../interfaces/IPost'
+import http from '../../service/api'
 
 const Home = () => {
+
+  const [posts, setPosts] = useState<IPost[]>([])
+
+  useEffect(() => {
+    http.get<IPost[]>('posts')
+      .then(resposta => setPosts(resposta.data.slice(0, 2)))
+  }, [])
+
   return (
-    <div>
-        Home
+    <div className="container-sm">
+      <Navbar />
+      <div className="container-sm text-center p-5">
+        <h1 className="light-300">The Food Ninja Blog</h1>
+        <p className="roboto">A blog about food, experiences, and recipes.</p>
+      </div>
+      <div className="container-sm d-flex justify-content-center">
+        <Input />
+      </div>
+      <section>
+        {posts.map(post => {
+          return (
+            <div key={post.id}>
+              <p>{post.title}</p>
+              <p>{post.body}</p>
+            </div>
+          )
+        })}
+      </section>
     </div>
   )
 }
