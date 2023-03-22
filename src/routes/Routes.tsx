205c6343d85fe5607965fe5655be15ad03995578
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-22 00:25:42 -0300

    ✨ feat: criado rotas

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route } from 'react-router'
+import { BrowserRouter, Routes as Switch} from 'react-router-dom'
+import Home from '../pages/Home/Home'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+    <Switch>
+        <Route path='/' element={<Home/>}/>
+    </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes
