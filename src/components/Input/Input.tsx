a53162e9f769818f94c1bef98936784455b3cdae
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-22 03:50:46 -0300

    ✨ feat: criado componente de input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,10 @@
+
+const Input = () => {
+    return (
+        <div>
+            <input type="text" className="form-control" id="input1" placeholder="Search for posts" />
+        </div>
+    )
+}
+
+export default Input
