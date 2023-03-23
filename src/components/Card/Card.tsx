a40dcf49698366b088044e72509260c565562130
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-23 03:06:17 -0300

    ✨ feat: import de imagens agora é pelo id do post

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,15 +1,18 @@
-import image from '../../assets/images/1.jpg'
 
 interface DadosCard {
+    id: number
     title: string
     body: string
 }
+type URL = string;
+const IMAGES_PATH: URL = '/src/assets/images/'
 
-const Card = ({title, body}: DadosCard) => {
+const Card = ({title, body, id}: DadosCard) => {
+
     return (
         <div className="col h-100">
             <div className="card h-100">
-                <img src={image} className="img-fluid" alt="..."/>
+                <img src={IMAGES_PATH + `${id}.jpg`} className="img-fluid" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title bold-700">{title}</h5>
                     <p className="card-text regular-body">{body}... <strong>Leia mais!</strong></p>
