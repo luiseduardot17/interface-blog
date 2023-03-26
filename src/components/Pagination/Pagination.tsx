c5dc1b6edb4651d92f0d5a46fc8f4d30de91ed42
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-26 17:25:38 -0300

    ✨ feat: criado componente de paginação e suas props

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,92 @@
+
+
+interface PaginationProps {
+    totalPages: number;
+    currentPage: number;
+    onPageChange: (pageNumber: number) => void;
+}
+
+const Pagination = ({ totalPages, currentPage, onPageChange }: PaginationProps) => {
+
+    const renderPages = () => {
+        const pages = [];
+        const totalPagesToShow = 5;
+
+        if (totalPages <= totalPagesToShow) {
+            for (let i = 1; i <= totalPages; i++) {
+                pages.push(
+                    <li className={`page-item ${i === currentPage ? "active" : ""}`} key={i}>
+                        <button className="page-link" onClick={() => onPageChange(i)}>
+                            {i}
+                        </button>
+                    </li>
+                );
+            }
+        } else {
+            let start = currentPage - Math.floor(totalPagesToShow / 2);
+            let end = currentPage + Math.floor(totalPagesToShow / 2);
+            if (start < 1) {
+                start = 1;
+                end = start + totalPagesToShow - 1;
+            }
+            if (end > totalPages) {
+                end = totalPages;
+                start = end - totalPagesToShow + 1;
+            }
+            if (start > 1) {
+                pages.push(
+                    <li className="page-item" key="1">
+                        <button className="page-link" onClick={() => onPageChange(1)}>
+                            1
+                        </button>
+                    </li>
+                );
+                if (start > 2) {
+                    pages.push(
+                        <li className="page-item disabled" key="...">
+                            <span className="page-link">...</span>
+                        </li>
+                    );
+                }
+            }
+            for (let i = start; i <= end; i++) {
+                pages.push(
+                    <li className={`page-item ${i === currentPage ? "active" : ""}`} key={i}>
+                        <button className="page-link" onClick={() => onPageChange(i)}>
+                            {i}
+                        </button>
+                    </li>
+                );
+            }
+            if (end < totalPages) {
+                if (end < totalPages - 1) {
+                    pages.push(
+                        <li className="page-item disabled" key="...">
+                            <span className="page-link">...</span>
+                        </li>
+                    );
+                }
+                pages.push(
+                    <li className="page-item" key={totalPages}>
+                        <button className="page-link" onClick={() => onPageChange(totalPages)}>
+                            {totalPages}
+                        </button>
+                    </li>
+                );
+            }
+        }
+        return pages;
+    };
+
+    return (
+        <div>
+            <nav>
+                <ul className="pagination justify-content-center">
+                    {renderPages()}
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Pagination
