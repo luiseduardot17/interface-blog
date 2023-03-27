64a472310851639b7249b8e61b29269f61047854
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-27 13:35:32 -0300

    ✨ feat: add link que leva a pagina do autor

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
 
 interface DadosCard {
     id: number
     title: string
     body: string
+    authorName: string
+    link: number
 }
 type URL = string;
 const IMAGES_PATH: URL = '/src/assets/images/'
 
-const Card = ({title, body, id}: DadosCard) => {
+const Card = ({title, body, id, authorName, link}: DadosCard) => {
 
     return (
         <div className="col h-100">
@@ -16,6 +19,9 @@ const Card = ({title, body, id}: DadosCard) => {
                 <div className="card-body">
                     <h5 className="card-title bold-700">{title}</h5>
                     <p className="card-text regular-body">{body}... <strong>Read more!</strong></p>
+                    <div className="d-flex">
+                    <Link to={`/users/${link}`} className="nav-link position-absolute bottom-0 start-0"><i className="bi bi-person-circle" style={{ color: "#F39C12", paddingLeft: 15 + "px"}}></i> {authorName}</Link>
+                    </div>
                 </div>
             </div>
         </div>
