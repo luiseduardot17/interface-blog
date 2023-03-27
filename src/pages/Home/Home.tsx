bfa7ef6ccbf55bce38bf82d2c3b54014652fb7f1
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-27 13:35:01 -0300

    ✨ feat: agora é possivel ver o autor do post

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,11 +6,13 @@ import Input from '../../components/Input/Input'
 import Navbar from '../../components/Navbar/Navbar'
 import Pagination from '../../components/Pagination/Pagination'
 import IPost from '../../interfaces/IPost'
+import IUsers from '../../interfaces/IUsers'
 import http from '../../service/api'
 
 const Home = () => {
   const [posts, setPosts] = useState<IPost[]>([])
   const [searchText, setSearchText] = useState("")
+  const [users, setUsers] = useState<IUsers[]>([])
 
   const [currentPage, setCurrentPage] = useState<number>(1)
   const postsPerPage = 4;
@@ -18,6 +20,9 @@ const Home = () => {
   useEffect(() => {
     http.get<IPost[]>('posts')
       .then(resposta => setPosts(resposta.data))
+    http.get<IUsers[]>(`users`)
+      .then(resposta => setUsers(resposta.data)
+      )
   }, [])
 
   function handlePageChange(pageNumber: number) {
@@ -34,37 +39,39 @@ const Home = () => {
 
   return (
     <>
-    <div className="container-sm">
-      <Navbar />
-      <div className="container-sm text-center p-5">
-        <h1 className="light-300">The Food Ninja Blog</h1>
-        <p className="roboto">A blog about food, experiences, and recipes.</p>
-      </div>
-      <div className="container-sm d-flex justify-content-center">
-        <Input value={searchText} onChange={handleSearchChange} />
-      </div>
-      <section className='container-sm pt-5 pb-5'>
-        <div className="row row-cols-1 row-cols-md-2 g-5 justify-content-around">
-          {filteredPostsPerPage.map(post => {
-            return (
-              <Link to={`/post/${post.id}`} className="react-link" key={post.id}>
-                <Card title={post.title} body={post.body.slice(0, 100)} id={post.id} />
-              </Link>
-            )
-          })}
+      <div className="container-sm">
+        <Navbar />
+        <div className="container-sm text-center p-5">
+          <h1 className="light-300">The Food Ninja Blog</h1>
+          <p className="roboto">A blog about food, experiences, and recipes.</p>
         </div>
-      </section>
+        <div className="container-sm d-flex justify-content-center">
+          <Input value={searchText} onChange={handleSearchChange} />
+        </div>
+        <section className='container-sm pt-5 pb-5'>
+          <div className="row row-cols-1 row-cols-md-2 g-5 justify-content-around">
+            {filteredPostsPerPage.map(post => {
+              const author = users.find(user => user.id === post.userId);
+              const authorName = author ? author.name : 'Anonymous author';
+              return (
+                <Link to={`/post/${post.id}`} className="react-link" key={post.id}>
+                  <Card title={post.title} body={post.body.slice(0, 100)} id={post.id} authorName={authorName} link={post.userId} />
+                </Link>
+              )
+            })}
+          </div>
+        </section>
 
-      <Pagination
-        totalPages={Math.ceil(filteredPosts.length / postsPerPage)}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-      />
-    </div>
-    
-    <div className="container-fluid">
-    <Footer/>
-    </div>
+        <Pagination
+          totalPages={Math.ceil(filteredPosts.length / postsPerPage)}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      </div>
+
+      <div className="container-fluid">
+        <Footer />
+      </div>
     </>
   )
 }
