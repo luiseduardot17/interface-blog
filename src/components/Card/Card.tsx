e8303aff76292546325e7627b2fff1b3c952a675
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-29 17:37:57 -0300

    🐛 fix: removido link que causava erro de tags e semantica

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,14 @@
-import { Link } from "react-router-dom";
 
 interface DadosCard {
     id: number
     title: string
     body: string
     authorName: string
-    link: number
 }
 type URL = string;
 const IMAGES_PATH: URL = '/src/assets/images/'
 
-const Card = ({title, body, id, authorName, link}: DadosCard) => {
+const Card = ({title, body, id, authorName}: DadosCard) => {
 
     return (
         <div className="col h-100">
@@ -19,8 +17,8 @@ const Card = ({title, body, id, authorName, link}: DadosCard) => {
                 <div className="card-body">
                     <h5 className="card-title bold-700">{title}</h5>
                     <p className="card-text regular-body">{body}... <strong>Read more!</strong></p>
-                    <div className="d-flex">
-                    <Link to={`/users/${link}`} className="nav-link position-absolute bottom-0 start-0"><i className="bi bi-person-circle" style={{ color: "#F39C12", paddingLeft: 15 + "px"}}></i> {authorName}</Link>
+                    <div className="d-flex align-items-center position-absolute bottom-0 start-0">
+                    <i className="bi bi-person-circle" style={{ color: "#F39C12", paddingLeft: 15 + "px", paddingRight: 10}}></i><p className="regular-body mb-0"> {authorName}</p>
                     </div>
                 </div>
             </div>
