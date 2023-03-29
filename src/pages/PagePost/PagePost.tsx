4e23183c9fd92c07b76939ed5e28988b701fdebe
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-29 17:41:52 -0300

    ✨ feat: agora os posts possuem data, tempo de leitura, e o autor

--- src/pages/PagePost/PagePost.tsx
+++ src/pages/PagePost/PagePost.tsx
@@ -2,9 +2,11 @@ import Navbar from "../../components/Navbar/Navbar"
 import IPost from "../../interfaces/IPost"
 import { useEffect, useState } from "react"
 import http from "../../service/api"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import IComments from "../../interfaces/IComments"
 import Footer from "../../components/Footer/Footer"
+import IUsers from "../../interfaces/IUsers"
+import { format } from 'date-fns';
 
 type URL = string;
 const IMAGES_PATH: URL = '/src/assets/images/'
@@ -14,9 +16,21 @@ const PagePost = () => {
 
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
+  const [userId, setUserId] = useState(Number)
+  const [date, setDate] = useState(Date)
   const [post, setPost] = useState<IPost[]>([])
+  const [users, setUsers] = useState<IUsers[]>([])
   const [comentarios, setComentarios] = useState<IComments[]>()
   const [showForm, setShowForm] = useState(false);
+  const [readingTime, setReadingTime] = useState(0);
+
+  const startDate = new Date('2022-12-01');
+  const endDate = new Date();
+  const randomDate = getRandomDate(startDate, endDate);
+  const formattedDate = format(randomDate, 'MMM d, yyyy');
+
+  const author = users.find(user => user.id === userId);
+  const authorName = author ? author.name : 'Anonymous author';
 
   useEffect(() => {
     if (parametro.id) {
@@ -26,11 +40,34 @@ const PagePost = () => {
       http.get<IPost>(`posts/${parametro.id}/`)
         .then(resposta => setBody(resposta.data.body)
         )
+      http.get<IPost>(`posts/${parametro.id}/`)
+        .then(resposta => setUserId(resposta.data.userId)
+        )
+      http.get<IUsers[]>(`users`)
+        .then(resposta => setUsers(resposta.data)
+        )
       http.get<IComments[]>(`posts/${parametro.id}/comments`)
         .then(resposta => setComentarios(resposta.data)
         )
+      setDate(formattedDate);
+      calculateReadingTime();
     }
   }, [parametro])
+
+  function getRandomDate(startDate: Date, endDate: Date): Date {
+    const startTimestamp = startDate.getTime();
+    const endTimestamp = endDate.getTime();
+    const randomTimestamp = startTimestamp + Math.random() * (endTimestamp - startTimestamp);
+    const randomDate = new Date(randomTimestamp);
+    return randomDate;
+  }
+
+  function calculateReadingTime() {
+    const content = body
+    const wordsPerMinute = 200;
+    const readingTime = Math.ceil(content.split(' ').length / wordsPerMinute);
+    setReadingTime(readingTime);
+  }
 
   function handleAddComment() {
     setShowForm(!showForm);
@@ -41,7 +78,11 @@ const PagePost = () => {
       <Navbar />
       <div className="container-sm p-5">
         <h2 className="card-title bold-700-title">{title}</h2>
+        <p className="regular-body">{formattedDate} • <span>{readingTime} min read</span></p>
         <img src={IMAGES_PATH + `${parametro.id}.jpg`} className="img-fluid rounded" alt="..." />
+        <div className="d-flex justify-content-start">
+          <i className="bi bi-person-circle" style={{ color: "#F39C12", fontSize: 3 + "rem", paddingRight: 10 }}></i><Link to={`/users/${userId}`} className="nav-link d-flex align-items-center"> {authorName}</Link>
+        </div>
         <p className="card-text body">{body}</p>
       </div>
       <section className='container-sm pt-5 pb-5'>
