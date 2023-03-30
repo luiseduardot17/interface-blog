7011088b35e4dc9e524f41be9da58986ceffebea
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-03-30 00:52:43 -0300

    Updates

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Routes from './routes/Routes'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
-)
